0e1ec9fe96aac7edd2e41ff7897b24c6f939290d
Alpaca <bardiavhd8002@gmail.com>
2023-02-09 15:16:00 +0330

    merged branch main with bardia.New methods added
    new methods:
    maxstringlength
    minstringlength
    complement
    newDFA(work in progress)
    Union,Difference,Intersection added(work in progress

--- Theory_of_computation_project.py
+++ Theory_of_computation_project.py
@@ -1,5 +1,6 @@
 #   TheoryOfComputationProject by Ali mirzaei and Bardia Vahedi
 import networkx as nx
+from collections import defaultdict, deque
 
 class DFA:
     def __init__(self):
@@ -71,7 +72,66 @@ class DFA:
             return True
         except:
             return False 
-
+        
+        
+    def maxstringlength(self):
+        if self.isNull(): return 0
+        if not self.isFinite(): return "the language is not finite."
+        else:
+            graph=nx.DiGraph([(start_state, end_state)
+            for start_state, transition in self.transitions.items()
+            for end_state in transition.values()])
+            states=nx.descendants(graph, self.initial_state)
+            Reachable2Accept=self.accept_states.union(*(nx.ancestors(graph,state)for state in self.accept_states))
+            commonstates=states.intersection(Reachable2Accept)
+            sub=graph.subgraph(commonstates)
+            return nx.dag_longest_path_length(sub) 
+            
+    def minstringlength(self):
+        queue = deque()
+        distances = defaultdict(lambda: None)
+        distances[self.initial_state] = 0
+        queue.append(self.initial_state)
+        while queue:
+            state = queue.popleft()            
+            if state in self.accept_states:
+                return distances[state]
+            for next_state in self.transitions[state].values():
+                if distances[next_state] is None:
+                    distances[next_state] = distances[state] + 1
+                    queue.append(next_state)
+        return 0
+        
+    def Complement(self):
+        NewAccept=set()
+        for state in self.states:
+            if state not in self.accept_states: NewAccept.add(state)
+        NewDFA=DFA(self.states,self.alphabets,self.initial_state,self.transitions, NewAccept)
+        return NewDFA
+        
+    def NewDFA(dfa1,dfa2):
+        if dfa1.alphabets != dfa2.alphabets: raise Exception('input symbols do not match!')
+        newinitial=dfa1.initial_state+'_'+dfa2.initial_state
+        usefulstates={newinitial}
+        visited=[newinitial]
+        while len(visited)>0:
+            state=visited.pop(0)
+            for symbol in dfa1.alphabets:
+                nextdfastate=dfa1.transitions[state.split('_')[0]][symbol]+'_'+dfa2.transitions[state.split('_')[1]][symbol]
+                usefulstates.add(nextdfastate)
+                visited.append(nextdfastate)
+                    
+        return usefulstates,newinitial
+                
+        
+    def Union(self,other)->self:
+        pass
+        
+    def Difference(self,other)->self:
+        pass
+        
+    def Intersection(self,other)->self:
+        pass
 
 
 
