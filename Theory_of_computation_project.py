af06d9fb1698e82d3db8baa8f5d5408a50f8a0ea
Alpaca <bardiavhd8002@gmail.com>
2023-02-09 04:09:48 +0330

    merged two branches and solved conflicts.

--- Theory_of_computation_project.py
+++ Theory_of_computation_project.py
@@ -36,39 +36,42 @@ class DFA:
         print("accept_states are: ", self.accept_states)
         print("transitions are:   ", self.transitions)
 
-    def isAccept(self, string):
-
-        self.current_state = self.start_state
-
-        for char in string:
-            self.current_state = self.transitions[(self.current_state, char)]
-        if (self.current_state in self.accept_states):
-            return True
-        else:
+    def checkstring(self, string2check:str):
+            state=self.initial
+            for symbol in string2check:
+                state=self.transition[state][symbol]
+                print(state)
+            if state in self.Accept: return True
             return False
-
-    def isEmpty(self):
-        Reachables = []
-        queue = []
-        Reachables.append(self.start_state)
-        queue.append(self.start_state)
-        isempty = True
-
-        while queue:
-            state = queue.pop(0)
-
-            for next_state in self.transitions[state].values():
-                if next_state not in Reachables:
-                    if next_state in self.accept_states:
-                        isempty = False
-                    Reachables.append(next_state)
-                    queue.append(next_state)
-
-        return isempty
-
-    def isInfinite(self):
-        for test_string in range(len(self.states), len(self.states)*2):
-            pass
+        
+    def NullCheck(self):
+        visited=[self.initial]
+        states=[self.initial]
+        while len(states)>0:
+            state=states[0]
+            del states[0]
+            for next_state in self.transition[state].values():
+                if next_state not in visited: 
+                    if next_state in self.Accept: return False
+                    visited.append(next_state)
+                    states.append(next_state)
+        return True
+            
+    def FiniteCheck(self):
+        if self.NullCheck(): return True
+        graph=netx.DiGraph([(start_state, end_state)
+        for start_state, transition in self.transitions.items()
+        for end_state in transition.values()])
+        states=netx.descendants(graph, self.initial)
+        Reachable2Accept=self.Accept.union(*(netx.ancestors(graph,state)for state in self.Accept))
+        commonstates=states.intersection(Reachable2Accept)
+        sub=graph.subgraph(commonstates)
+            
+        try:
+            longest=netx.dag_longest_path_length(sub)
+            return True
+        except:
+            return False 
 
 
 
